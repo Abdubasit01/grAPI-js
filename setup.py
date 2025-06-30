7668c41aa2a38a28b68bee3afcc3d79f6a661319
GhostNinja <121746490+DghostNinja@users.noreply.github.com>
2025-06-30 08:08:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="grapi",
-    version="0.1.0",
-    author="iPsalmy",
-    description="Interactive API endpoint grabber for web pentesting",
+    name='grAPI',
+    version='0.1.0',
+    description='An API discovery tool using Playwright and JS scanning',
+    author='iPsalmy',
+    author_email='ipsalmy@gmail.com',
+    url='https://github.com/DghostNinja/grAPI',
     packages=find_packages(),
-    install_requires=[
-        "playwright>=1.43.0"
-    ],
+    install_requires=['playwright'],
     entry_points={
-        "console_scripts": [
-            "grapi=grapi.cli:main"
+        'console_scripts': [
+            'grapi = grapi.core:main'
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Intended Audience :: Developers",
-        "Topic :: Security",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    python_requires=">=3.7",
+    python_requires='>=3.7',
 )
